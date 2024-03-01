f6acc4f7936e767e200878a25159af618136782c
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2024-03-01 14:28:31 +0700

    Add files via upload

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(int argc,char** argv){
+	int N = 0;
+	if(argc == 1){
+	cout << "Please input numbers to find average.";
+	}
+	else{ for(int i = 1 ;i<=(argc-1);i++){
+		N += atoi(argv[i]);
+		}
+		float avg = N/(argc-1);
+		cout << "---------------------------------\n";
+		cout << "Average of " << (argc-1) << " numbers = " << avg;
+		cout << "\n---------------------------------";
+		
+	}
+}
